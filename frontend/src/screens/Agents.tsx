bf7f019b8789985aa923e0c08b852fbd2d7f63d4
alvin-zhaf <axz417@student.bham.ac.uk>
2025-04-12 20:20:38 +0100

    improvements on agents page

--- frontend/src/screens/Agents.tsx
+++ frontend/src/screens/Agents.tsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 // Basic styling for the page
 const Container = styled.div`
-  padding: 2rem;
-  background-color: #f5f5f5;
+  background-color: #18181b;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100vw;
 `;
 
 const AgentListContainer = styled.div`
